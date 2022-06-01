0534b4bf056c09eb34b383851d1dac5805c8bd59
August-cloud <65202579+August-cloud@users.noreply.github.com>
2022-05-31 17:43:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,74 @@
-from flask import Flask, request
-import telegram
-import os
-TOKEN = os.getenv('BOT_TOKEN')
-URL = os.getenv('URL')
-bot = telegram.Bot(token=TOKEN)
-
-app = Flask(__name__)
-
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
-
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
+from telegram import Bot
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
   
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = "Hi! I respond by echoing messages. Give it a try!"
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+updater = Updater("5522845545:AAF276GkvYD-NYtlDfaGAsyFNAqcLg8LV6Y",
+                  use_context=True)
+  
+  
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "End time warriors is a global network with the vision of raising an army of God(Obadiah 1:21) from generation to generation to change and transform through the power of the Holy Spirit. An army going to bring into alignment anything not in the programming of Elohim. Jeremiah 1:9-12. And to cause the influence of Christ spread across the globe through all platforms available.\
+        /help to see the commands available.")
+  
+def help(update: Update, context: CallbackContext):
+    update.message.reply_text("""Available Commands :-
+    /instagram - To get the instagram URL
+    /website - To get the URL for our website
+    /gmail - Send us email
+    /meeting days  - To get the endtime warriors telegram link""")
+  
+    
+def gmail_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "endtimewarroirs4@gmail.com")
+  
+  
+def instagram_url(update: Update, context: CallbackContext):
+    update.message.reply_text("Youtube Link =>\
+    https://instagram.com/_endtimewarriors?r=nametag")
+  
+  
+def website_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Website still in development")
+  
+  
+def meetings_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Link to telegram group: https://t.me/endtimewarriorsprayergrp or Subscribe to our channel: https://t.me/endtimewarriors4\
+            /Meeting Days ====== > Wednesdays: Prophetic Service @ 7pm\
+                / Fridays: Prayer Night(Night of transformation and encounter @ 9pm)\
+                    Saturdays: Unlocking the Bible @ 6:30pm\
+                        You are warmly welcome to join us as we worship")
+    
+  
+  
+def unknown(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry '%s' is not a valid command" % update.message.text)
+  
+  
+def unknown_text(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry I can't recognize you , you said '%s'" % update.message.text)
 
-   else:
-       bot.sendMessage(chat_id=chat_id, text=text)
-
-   return 'ok'
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    # we use the bot object to link the bot to our app which live
-    # in the link provided by URL
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    # something to let us know things work
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
-
-@app.route('/')
-def index():
-    return 'Hello, welcome to the telegram bot index page'
-
-if __name__ == '__main__':
-    # note the threaded arg which allow
-    # your app to have more than one thread
-    app.run(threaded=True)
+  
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('instagram', instagram_url))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('website', website_url))
+updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
+updater.dispatcher.add_handler(CommandHandler('meeting days', meetings_url))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+    Filters.command, unknown))  # Filters out unknown commands
+  
+# Filters out unknown messages.
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+  
+updater.start_polling()
